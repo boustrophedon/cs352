46ba850acfe198ede06abc67406ff3e9845b1b3d
Harry Stern <hcs@meow.sh>
2018-02-09 19:15:36 -0500

    Add provided skeleton code

---
+++ 2/sock352.py
@@ -0,0 +1,26 @@
+
+# This is the skeleton code of a cs 352 socket
+# You must change the code in the pass statements to make the client and server work. 
+
+import socket as ip
+
+class socket:
+    
+    def __init__(self):
+        pass
+    
+    def socket():
+        pass
+    
+    def bind(self,address):
+        pass 
+    
+    def sendto(self,buffer,address):
+        pass 
+
+    def recvfrom(self,nbytes):
+        pass
+
+    def close(self):
+        pass
+
