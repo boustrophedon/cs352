3eecfd6985cd4d31671a3ae3f0a5b837d7adaf02
Harry Stern <hcs@meow.sh>
2018-02-24 01:48:14 -0500

    Fix test cases' header spaces and tests
    - Sort order for lines with the same count seems arbitrary so I had to
    change the way the test works

--- 3/test.py
+++ 3/test.py
@@ -1,16 +1,22 @@
 import unittest
 import wireshark1
 
+from itertools import izip_longest
 
 class TestWireshark1(unittest.TestCase):
     def compare_to_file(self, pcap, expected_file):
-        actual_report = wireshark1.parse_packets(pcap)
+        actual_report = wireshark1.parse_packets(pcap).split('\n')
         self.assertTrue(len(actual_report) > 0)
 
         with open(expected_file) as f:
             expected_report = [line.strip() for line in f]
-            for expected, actual in zip(expected_report, actual_report.split('\n')):
-                self.assertEqual(expected, actual)
+
+            for actual in actual_report:
+                # because the sort order is arbitrary for lines with equal
+                # counts we have to do this shitty test and then test that the
+                # number of lines are the same
+                self.assertIn(actual, expected_report)
+            self.assertEqual(len(actual_report), len(expected_report))
 
     def test_small(self):
         self.compare_to_file("small.pcap", "output-small.txt") 
