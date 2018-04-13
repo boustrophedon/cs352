5b5789ac08683c9056656ba0f9ceeabb49efc734
Harry Stern <hcs@meow.sh>
2018-04-13 02:52:22 -0400

    tmp

--- 4/wireshark_02.py
+++ 4/wireshark_02.py
@@ -5,23 +5,68 @@ import datetime
 import socket
 import argparse
 
-class Probes(object):
-    def __init__(self, w, n):
-        self.w = w
-        self.n = n
-        self.probes = dict()
+from abc import ABCMeta, abstractmethod
 
-    def add(self, packet, timestamp):
+class PacketCluster(object):
+    __metaclass__ = ABCMeta
+    def __init__(self, tcp_packets, udp_packets):
+        self.tcp_packets = list(tcp_packets)
+        self.udp_packets = list(udp_packets)
+
+        self.tcp_packets.sort(key=lambda x: self.feature(x))
+        self.udp_packets.sort(key=lambda x: self.feature(x))
+
+    @abstractmethod
+    def feature(self, packet):
         pass
 
-class Scans(object):
-    def __init__(self, w, n):
-        self.w = w
-        self.n = n
-        self.scans = dict()
+    def find_clusters(self, w, n):
+        tcp_clusters = self._find_clusters(self.tcp_packets, w, n)
+        udp_clusters = self._find_clusters(self.udp_packets, w, n)
 
-    def add(self, packet, timestamp):
-        pass
+        return (tcp_clusters, udp_clusters)
+
+    def _find_clusters(self, packets, w, n):
+        clusters = list()
+        if len(packets) == 0:
+            return clusters
+            
+        curr = list()
+        for packet in packets:
+            if not curr:
+                curr.append(packet)
+                continue
+            
+            if (self.feature(packet) - self.feature(curr[-1])) <= w:
+                curr.append(packet)
+            else:
+                if len(curr) >= n:
+                    clusters.append(list(curr))
+                    del curr[:]
+
+        # we might have ended on a cluster
+        if len(curr) >= n:
+            clusters.append(list(curr))
+            del curr[:]
+        return clusters
+
+class Probes(PacketCluster):
+    def __init__(self, tcp_packets, udp_packets):
+        super(Probes, self).__init__(tcp_packets, udp_packets)
+
+    def feature(self, packet):
+        # port
+        return packet[1].dport
+
+class Scans(PacketCluster):
+    def __init__(self, tcp_packets, udp_packets):
+        super(Scans, self).__init__(tcp_packets, udp_packets)
+
+    def feature(self, packet):
+        # timestamp
+        return packet[0]
+
+
 
 # convert IP addresses to printable strings 
 def inet_to_str(inet):
@@ -54,14 +99,30 @@ def main():
 
     input_data = dpkt.pcap.Reader(open(file_name,'r'))
 
-    probes = Probes()
-    scans = Scans()
-
+    tcp = list()
+    udp = list()
     for timestamp, packet in input_data:
-        
-        # your code goes here ...
-        probes.add(packet, timestamp)
-        scans.add(packet, timestamp)
+        eth = dpkt.ethernet.Ethernet(packet)
+        if type(eth.data) == dpkt.ip.IP:
+            ip = eth.data
+            if type(ip.data) == dpkt.tcp.TCP:
+                tcp.append((timestamp, ip.data))
+            if type(ip.data) == dpkt.udp.UDP:
+                udp.append((timestamp, ip.data))
+
+    probes = Probes(tcp, udp)
+    scans = Scans(tcp, udp)
+
+    tcp_probes, udp_probes = probes.find_clusters(W_p, N_p)
+    tcp_scans, udp_scans = scans.find_clusters(W_s, N_s)
+
+    print("Report for TCP")
+    print("Probes: {}".format(len(tcp_probes)))
+    print("Scans: {}".format(len(tcp_scans)))
+
+    print("Report for UDP")
+    print("Probes: {}".format(len(udp_probes)))
+    print("Scans: {}".format(len(udp_scans)))
 
 # execute a main function in Python
 if __name__ == "__main__":
