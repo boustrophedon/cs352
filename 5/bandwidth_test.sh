75803c688194f1a901d9acaad23f11ec037f1ad6
Harry Stern <hcs@meow.sh>
2018-05-01 20:41:01 -0400

    Add small test script and original skeleton code

---
+++ 5/bandwidth_test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python2 rel_bw_test.py -s -f rel_bw_test.py -d localhost -p 4000 -l 5000 &
+serv_pid=$!
+sleep 0.5
+python2 rel_bw_test.py -d localhost -p 5000 -l 3333 -f rel_client_1.py
---
+++ 5/digest_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python2 rel_server_1.py -l 5000 &
+serv_pid=$!
+sleep 0.5
+python2 rel_client_1.py -d localhost -p 5000 -l 3333 -f rel_client_1.py
+
+kill $serv_pid
